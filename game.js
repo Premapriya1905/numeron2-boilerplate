2e78486dd0cb018a31c8727862c92975677e9d4c
Premapriya1905 <Premapriya1905@gmail.com>
2023-11-17 11:05:02 +0530

    numeron2

--- game.js
+++ game.js
@@ -1,11 +1,147 @@
-// Iteration 2: Generating two random numbers (0 to 100) and displaying the same in the game.html
+var num1 = Math.round(Math.random() * 100);
+var num2 = Math.round(Math.random() * 100);
 
-// Iteration 3: Creating variables required to make the game functional
+var num1div = document.getElementById("number1");
+var num2div = document.getElementById("number2");
 
-// Iteration 4: Creating a variable for number 3 and a variable for storing the html element with the Id "number3"
+var operation;
+var score=0;
+var plus = document.getElementById("plus");
+var minus = document.getElementById("minus");
+var mul = document.getElementById("mul");
+var divide = document.getElementById("divide");
+var modulus = document.getElementById("modulus");
 
-// Iteration 5: Creating a randomise function to make the game functional
+var num3;
+var num3div = document.getElementById("number3");
 
-// Iteration 6: Making the Operators (button) functional
+function randomise() {
+    num1 = Math.round(Math.random() * 100);
+    num2 = Math.round(Math.random() * 100);
+    var temp;
+    if (num1 < num2) {
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
 
-// Iteration 7: Making Timer functional
+    var operation=Math.round(Math.random()*5);
+    switch (operation) {
+        case 1:
+            num3 = num1 + num2;
+            break;
+        case 2:
+            num3 = num1 - num2;
+            break;
+        case 3:
+            num3 = num1 * num2;
+            break;
+        case 4:
+            num3 = Math.floor(num1 / num2);
+            break;
+        case 5:
+            num3 = num1 % num2;
+            break;
+        case 0:
+            randomise();
+    }
+
+    num1div.innerHTML = num1;
+    num2div.innerHTML = num2;
+    num3div.innerHTML = num3;
+}
+
+randomise();
+
+
+
+plus.addEventListener("click", () => {
+
+    if (num1 + num2 === num3) {
+        score++;
+        // console.log(score)
+        randomise();
+        resetTime(timerId); 
+
+    } else {
+        // console.log("check")
+        window.location.href = "gameover.html?score=" + score;
+    }
+});
+
+minus.addEventListener("click", () => {
+    if (num1 - num2 === num3) {
+        score++;
+        // console.log(score)
+
+        randomise();
+        resetTime(timerId);
+    } else {
+        // console.log("check1")
+
+        window.location.href = "gameover.html?score=" + score;
+    }
+});
+
+mul.addEventListener("click", () => {
+    if (num1 * num2 === num3) {
+        score++;
+        // console.log(score)
+
+        randomise();
+        resetTime(timerId)
+    } else {
+        // console.log("check2")
+
+        window.location.href = "gameover.html?score=" + score;
+    }
+});
+
+divide.addEventListener("click", () => {
+    if (Math.floor(num1 / num2) === num3) {
+        score++;
+        // console.log(score)
+
+        randomise();
+        resetTime(timerId);
+    } else {
+        // console.log("check3")
+
+        window.location.href ="gameover.html?score=" + score;
+    }
+});
+
+modulus.addEventListener("click", () => {
+    if (num1 % num2 === num3) {
+        score++;
+        // console.log(score)
+
+        randomise();
+        resetTime(timerId);
+    } else {
+        // console.log("check4")
+
+        window.location.href = "gameover.html?score=" + score;
+    }
+});
+
+var time = 20;
+var timerId;
+
+function startTimer() {
+  time = 20;
+  timer.innerHTML = time;
+  timerId = setInterval(() => {
+    time--;
+    if (time == 0) location.href = "./gameover.html?score=" + score;
+    timer.innerHTML = time;
+  }, 1000);
+  localStorage.setItem("score",score);
+}
+
+function resetTime(intervalId) {
+  clearInterval(intervalId);
+  startTimer();
+}
+
+startTimer();
--- score.js
+++ score.js
@@ -1,1 +1,13 @@
 // Iteration 8: Making scoreboard functional
+var scorediv = document.getElementById("score-board");
+
+let score = localStorage.getItem("score");
+
+const playagain = document.getElementById('play-again-button');
+scorediv.innerHTML = score;
+playagain.addEventListener("click", () => {
+    location.href = "./game.html";
+});
+
+
+
